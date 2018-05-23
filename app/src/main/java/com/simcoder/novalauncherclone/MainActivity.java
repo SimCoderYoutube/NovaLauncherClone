75de69b313f5bcb360bc5bdee604ada5dfda751e
SimCoder <simplecodervideos@gmail.com>
2018-05-23 19:55:24 +0100

    Lesson4 - App Drawer Scroll Fix

--- app/src/main/java/com/simcoder/novalauncherclone/MainActivity.java
+++ app/src/main/java/com/simcoder/novalauncherclone/MainActivity.java
@@ -3,17 +3,21 @@ package com.simcoder.novalauncherclone;
 import android.content.Intent;
 import android.content.pm.ResolveInfo;
 import android.graphics.drawable.Drawable;
+import android.support.annotation.NonNull;
 import android.support.design.widget.BottomSheetBehavior;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.GridView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
+    boolean isBottom = true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,13 +33,28 @@ public class MainActivity extends AppCompatActivity {
     private void initializeDrawer() {
         View mBottomSheet =findViewById(R.id.bottomSheet);
         final GridView mDrawerGridView = findViewById(R.id.drawerGrid);
-        BottomSheetBehavior mBottomSheetBehavior = BottomSheetBehavior.from(mBottomSheet);
+        final BottomSheetBehavior mBottomSheetBehavior = BottomSheetBehavior.from(mBottomSheet);
         mBottomSheetBehavior.setHideable(false);
         mBottomSheetBehavior.setPeekHeight(300);
 
         installedAppList = getInstalledAppList();
 
         mDrawerGridView.setAdapter(new AppAdapter(getApplicationContext(), installedAppList));
+
+        mBottomSheetBehavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View bottomSheet, int newState) {
+                if(newState == BottomSheetBehavior.STATE_HIDDEN && mDrawerGridView.getChildAt(0).getY() != 0)
+                    mBottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+                if(newState == BottomSheetBehavior.STATE_DRAGGING && mDrawerGridView.getChildAt(0).getY() != 0)
+                    mBottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+            }
+
+            @Override
+            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+            }
+        });
+
     }
 
     private List<AppObject> getInstalledAppList() {
@@ -53,7 +72,6 @@ public class MainActivity extends AppCompatActivity {
             AppObject app = new AppObject(appPackageName, appName, appImage);
             if (!list.contains(app))
                 list.add(app);
-            AppObject appsingle = new AppObject(app)
         }
         return list;
     }
